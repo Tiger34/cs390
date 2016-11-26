83f52ee10358544f5ee4a841f2e1ee2b4f04c3ae
Tiger34 <justinwatkins114@icloud.com>
2016-11-26 12:32:10 -0500

    Add files via upload
    CS390 Note assignment

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="CS390.css">
+  <title>CS390</title>
+</head>
+<body>
+<h1>CS390</h1>
+<table>
+
+<tr><td>
+	<a href="http://www.brianiosdev.com/CS390/2-11%20&%202-16.pdf">Chomsky</a><a href="http://www.brianiosdev.com/CS390/2-18%20Notes%20Review%202.pdf">Grammars</a><a href="http://www.brianiosdev.com/CS390/2-18%20Notes%20Review.pdf">Grammars2</a><a href="http://www.brianiosdev.com/CS390/2-9%20week.pdf">Grammars3</a><a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20May%2028,%202016,%204_21_19%20PM.pdf">Grammar4</a>
+
+</td></tr>
+
+<tr><td>
+<a href="http://www.brianiosdev.com/CS390/Cs390%201-3.pdf">Optinal-final</a><a href="http://www.brianiosdev.com/CS390/March%201st.pdf">TestQ</a><a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20Apr%202,%202016,%203">palindrone</a><a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20Mar%2028,%202016,%209_13_00%20PM.pdf">palindrone2</a>
+<a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20Mar%2030,%202016,%2011_17_46%20PM.pdf">Review</a>
+</td><tr>
+
+<tr><td>
+<a href="http://www.brianiosdev.com/CS390/CS%20390%20vid%204.pdf">CS390vid4</a>
+<a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20May%204,%202016,%208_49_19%20PM.pdf">Recursive-func</a>
+<a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20May%204,%202016,%208_50_20%20PM.pdf">CS390-2</a>
+<a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20May%204,%202016,%208_52_01%20PM.pdf">Salesman</a>
+
+</tr></td>
+
+<tr><td>
+<a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20May%204,%202016,%208_55_00%20PM.pdf">test-re</a>
+<a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20May%204,%202016,%209_01_27%20PM.pdf">Reveiw</a>
+<a href="http://www.brianiosdev.com/CS390/Scannable%20Document%20on%20May%205,%202016,%209_57_55%20AM.pdf">Final-exam</a>
+<a href="http://www.brianiosdev.com/CS390/Video%205.pdf">NFA</a>
+
+</tr></td>
+
+
+
+
+
+
+
+</table>
+<table>
+<h2>Downloadable Note Images</h2>
+<tr>
+<td>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%201.tiff">Pic 1</a>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%2010.tiff">Pic 2</a>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%2011.tiff">Pic 3</a>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%2013.tiff">Pic 4</a>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%2014.tiff">Pic 5</a>
+</td>
+</tr>
+<tr>
+<td>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%205.tiff">Pic 6</a>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%206.tiff">Pic 7</a>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%207.tiff">Pic 8</a>
+<a href="http://www.brianiosdev.com/CS390/Pasted%20Graphic%208.tiff">Pic 9</a>
+</td>
+</tr>
+</table>
+
+
+</body>
+</html>
